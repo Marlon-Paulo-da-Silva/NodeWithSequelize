2a300b473c1b85e2120f90bc75f88ce1d0f14c9c
Marlon Paulo <marlon.paulo.silva@outlook.com>
2020-01-13 09:07:38 -0300

    criando arquivo  sequelizerc para definir os caminhos do config e migrations

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ module.exports = {
   host: "rajje.db.elephantsql.com",
   username: "cetoujpd",
   password: '3u7f-MHnMz7TwnAyCC0GCXBSiBBJfuwf',
+  database: 'cetoujpd',
   define{
       timestamps: 'true',
       underscored: 'true'
