faba62ec8e004a39590ecac2f529488c0b57e770
Marlon Paulo <marlon.paulo.silva@outlook.com>
2020-01-13 08:59:02 -0300

    criando arquivo de configuracao database.js e a conexao database/index

---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dialect: "postgres",
+  host: "rajje.db.elephantsql.com",
+  username: "cetoujpd",
+  password: '3u7f-MHnMz7TwnAyCC0GCXBSiBBJfuwf',
+  define{
+      timestamps: 'true',
+      underscored: 'true'
+  }
+};
